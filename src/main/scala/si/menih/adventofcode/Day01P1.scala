dfbc2118a01b268e392aba3f4d523d8f7af407b4
Gregor Menih <gregor@menih.si>
2022-12-01 12:22:02 +0100

    feat: cleanup

--- src/main/scala/si/menih/adventofcode/Day01P1.scala
+++ src/main/scala/si/menih/adventofcode/Day01P1.scala
@@ -4,18 +4,18 @@ package Day1Part1
 
 import si.menih.adventofcode.lib.FileHelper
 
-def partitionInputs (inputs: Iterator[String]): List[List[Int]] =
+def partitionInputs (inputs: List[String]): List[List[Int]] =
   // println(inputs.toList)
   val (a, b) = inputs.span(_ != "")
   val rest = b.drop(1)
 
-  if (rest.isEmpty) List(a.map(_.toInt).toList)
-  else a.map(_.toInt).toList :: partitionInputs(rest)
+  if (rest.isEmpty) List(a.map(_.toInt))
+  else a.map(_.toInt) :: partitionInputs(rest)
 
 @main def Day01Part1(args: String*): Unit =
   val inputs = FileHelper.readInputLines("day01.txt")
 
-  val partitioned = partitionInputs(inputs)
+  val partitioned = partitionInputs(inputs.toList)
   val summed = partitioned.map(_.sum)
   val max = summed.max
 
