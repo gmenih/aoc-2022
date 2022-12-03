5047912ea2605f89515527a54cda172f5a342f45
Gregor Menih <gregor@menih.si>
2022-12-03 09:12:04 +0100

    Day 3

---
+++ src/main/scala/si/menih/adventofcode/Day03P1.scala
@@ -0,0 +1,30 @@
+package si.menih
+package adventofcode
+package Day3Part1
+
+import si.menih.adventofcode.lib.FileHelper
+
+object Rucksack:
+  def score (c: Char): Int =
+    if c >= 'A' && c <= 'Z' then
+      c - 'A' + 27
+    else
+      c - 'a' + 1
+
+  def splitInHalf (input: String): (String, String) =
+    val half = input.length / 2
+    (input.take(half), input.drop(half))
+
+  def solve (sack: String): Int =
+    val (left, right) = splitInHalf(sack)
+    val common = left.intersect(right).head
+    score(common)
+
+@main def Day03(args: String*): Unit =
+  val inputs = FileHelper.readInputLines("day03.txt")
+
+  var result = for (line <- inputs) yield Rucksack.solve(line)
+  val sum = result.sum
+
+  println(s"Result: $sum")
+
---
+++ src/main/scala/si/menih/adventofcode/Day03P2.scala
@@ -0,0 +1,28 @@
+package si.menih
+package adventofcode
+package Day3Part2
+
+import si.menih.adventofcode.lib.FileHelper
+
+object Rucksack:
+  def score (c: Char): Int =
+    if c >= 'A' && c <= 'Z' then
+      c - 'A' + 27
+    else
+      c - 'a' + 1
+
+  def solve (elves: Seq[String]): Int =
+    elves match
+      case Seq(a, b, c) =>
+        val common = a.intersect(b).intersect(c).head
+        score(common)
+      case _ => 0
+
+@main def Day03(args: String*): Unit =
+  val inputs = FileHelper.readInputLines("day03.txt")
+
+  val result = for (lines <- inputs.grouped(3)) yield Rucksack.solve(lines)
+  val sum = result.sum
+
+  println(s"Result: $sum")
+
