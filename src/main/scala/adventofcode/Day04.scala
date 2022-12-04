14fcacff6e016896f13f5da3e72405291c0a161a
Gregor Menih <gregor@menih.si>
2022-12-04 10:54:11 +0100

    Day 4

---
+++ src/main/scala/adventofcode/Day04.scala
@@ -0,0 +1,41 @@
+package si.menih
+package adventofcode
+package Day4Part1
+
+import si.menih.adventofcode.lib.FileHelper
+
+case class Pair (a: Int, b: Int)
+
+object UnPair:
+  val pattern = raw"(\d+)-(\d+),(\d+)-(\d+)".r
+
+  def parse (input: String): Option[(Pair, Pair)] =
+    input match
+      case pattern(a, b, c, d) => Some(Pair(a.toInt, b.toInt), Pair(c.toInt, d.toInt))
+      case _ => None
+
+  def toRanges (pairs: (Pair, Pair)): (Range, Range) =
+    (pairs._1.a to pairs._1.b, pairs._2.a to pairs._2.b)
+
+  def solvePart1 (pairs: (Pair, Pair)): Boolean =
+    val (r1, r2) = toRanges(pairs)
+    val intersection = r1.intersect(r2)
+    intersection.size >= r1.size || intersection.size >= r2.size
+
+  def solvePart2 (pairs: (Pair, Pair)): Boolean =
+    val (r1, r2) = toRanges(pairs)
+    r1.intersect(r2).nonEmpty
+
+@main def Day04(args: String*): Unit =
+  val inputs = FileHelper.readInputLines("day04.txt")
+
+  val (p1, p2) = inputs.map(UnPair.parse).flatten.duplicate
+  val part1 = p1.count(UnPair.solvePart1)
+  val part2 = p2.count(UnPair.solvePart2)
+
+  println(s"Part1: ${part1}")
+  println(s"Part2: ${part2}")
+
+
+
+
