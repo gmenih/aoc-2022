7db9bb10f9ffa90b8d800dafddf03c89ccb2164d
Gregor Menih <gregor@menih.si>
2022-12-11 22:56:58 +0100

    Dumb BigInt

--- src/main/scala/adventofcode/Day11P2.scala
+++ src/main/scala/adventofcode/Day11P2.scala
@@ -4,33 +4,31 @@ package Day11P2
 import si.menih.adventofcode.lib.FileHelper
 
 
-def gcd(a: BigInt, b: BigInt): BigInt =
-    if b == 0 then a else gcd(b, a % b)
-
-def lcm(a: BigInt, b: BigInt): BigInt =
-    a * b / gcd(a, b)
+// https://github.com/acmeism/RosettaCodeData/blob/master/Task/Least-common-multiple/Scala/least-common-multiple-1.scala
+def gcd(a: Long, b: Long): Long = if b==0 then a.abs else gcd(b, a % b)
+def lcm(a: Long, b: Long) = (a * b).abs / gcd(a, b)
 
 object MonkeyBusiness:
-  type WorryFn = (v: BigInt) => BigInt
+  type WorryFn = (v: Long) => Long
   type TestFn = (v: Int) => Boolean
 
 
-  class Monkey(val id: Int, val items: List[BigInt], worryFn: WorryFn, val mod: Long, yes: Int, no: Int, val inspections: Long):
+  class Monkey(val id: Int, val items: List[Long], worryFn: WorryFn, val mod: Long, yes: Int, no: Int, val inspections: Long):
 
-    def this () = this(0, List(), (v: BigInt) => v, 1, 0, 0, 0)
+    def this () = this(0, List(), (v: Long) => v, 1, 0, 0, 0)
 
     def setId (id: Int) = Monkey(id, items, worryFn, mod, yes, no, inspections)
     def clearItems (i: Int) = Monkey(id, List(), worryFn, mod, yes, no, inspections + i)
-    def addItems (item: List[BigInt]) = Monkey(id, items ++ item, worryFn, mod, yes, no, inspections)
+    def addItems (item: List[Long]) = Monkey(id, items ++ item, worryFn, mod, yes, no, inspections)
     def setWorry (worryFn: WorryFn) = Monkey(id, items, worryFn, mod, yes, no, inspections)
     def setMod (mod: Int) = Monkey(id, items, worryFn, mod, yes, no, inspections)
     def setYes(yes: Int) = Monkey(id, items, worryFn, mod, yes, no, inspections)
     def setNo(no: Int) = Monkey(id, items, worryFn, mod, yes, no, inspections)
 
-    def test (v: BigInt) = if v % mod == 0 then yes else no
+    def test (v: Long) = if v % mod == 0 then yes else no
 
-    def processItems (commonMultiplier: BigInt): (List[(BigInt, Int)], Monkey) =
-      val newItems = items.map(v => worryFn(v) % commonMultiplier).map(v => (v, test(v)))
+    def processItems (commonMultiple: Long): (List[(Long, Int)], Monkey) =
+      val newItems = items.map(v => worryFn(v) % commonMultiple).map(v => (v, test(v)))
       return (newItems, clearItems(newItems.size))
 
 
@@ -39,13 +37,13 @@ object MonkeyBusiness:
       input.map(_.trim).foldLeft(Monkey())((monkey, input) => {
         input match {
           case s"Monkey $s:" => monkey.setId(s.toInt)
-          case s"Starting items: $s" => monkey.addItems(s.split(", ").map(_.toLong).map(BigInt.apply).toList)
+          case s"Starting items: $s" => monkey.addItems(s.split(", ").map(_.toLong).toList)
           case s"Operation: new = old $op $s" => monkey.setWorry(s match {
-            case "old" => (v: BigInt) => v * v
+            case "old" => (v: Long) => v * v
             case v => op match {
-              case "*" => (v: BigInt) => v * s.toLong
-              case "+" => (v: BigInt) => v + s.toLong
-              case _ => (v: BigInt) => v
+              case "*" => (v: Long) => v * s.toLong
+              case "+" => (v: Long) => v + s.toLong
+              case _ => (v: Long) => v
             }
           })
           case s"Test: divisible by $s" => monkey.setMod(s.toInt)
@@ -56,12 +54,12 @@ object MonkeyBusiness:
       })
     })
 
-  def run (monkeys: IndexedSeq[Monkey], rounds: Int, commonMultiplier: BigInt): Long =
+  def run (monkeys: IndexedSeq[Monkey], rounds: Int, commonMultiple: Long): Long =
     Range(0, rounds).foldLeft(monkeys.toIndexedSeq)((ms, _) => {
       ms.foldLeft(ms)((list, m) => {
         // Me no likey
         val monkey = list(m.id)
-        val (items, newMonkey) = monkey.processItems(commonMultiplier)
+        val (items, newMonkey) = monkey.processItems(commonMultiple)
 
         items.foldLeft(list.updated(monkey.id, newMonkey))((list2, item) => {
           val (worry, i) = item
@@ -83,9 +81,9 @@ object MonkeyBusiness:
   val monkeys = MonkeyBusiness.parse(inputs).toIndexedSeq
 
 
-  val commonMultiplier = monkeys.foldLeft(BigInt(1))((p, m) => lcm(p, m.mod))
+  val commonMultiple = monkeys.foldLeft(1L)((p, m) => lcm(p, m.mod))
 
-  val result = MonkeyBusiness.run(monkeys, 10_000, commonMultiplier);
+  val result = MonkeyBusiness.run(monkeys, 10_000, commonMultiple);
 
   println(s"Result: $result")
 
